4b3fdfe61c1990784ec422955e9a9f7d67041ac7
Loke <shadowz8009@gmail.com>
2023-01-09 01:07:52 +0000

    Update 4 files

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -5,36 +5,42 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faUser, faMugHot } from '@fortawesome/free-solid-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
-
-
-
 const Sidebar = () => (
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={Logo} alt="logo" />
+    </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#FFFFFF" />
+      </NavLink>
 
-    <div className='nav-bar'>
-        <Link className='logo' to='/portfolio'>
-            <img src={Logo} alt="logo" />
-        </Link>
-        <nav>
-            <NavLink exact="true" activeclassname="active" to="/portfolio/">
-                <FontAwesomeIcon icon={faHome} color="#FFFFFF" />
-            </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="#FFFFFF" />
+      </NavLink>
 
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/portfolio/about">
-                <FontAwesomeIcon icon={faUser} color="#FFFFFF" />
-            </NavLink>
-
-            <NavLink exact="true" activeclassname="active" className="projects-link" to="/portfolio/projects">
-                <FontAwesomeIcon icon={faMugHot} color="#FFFFFF" />
-            </NavLink>
-        </nav>
-        <ul>
-            <li>
-                <a target="_blank" rel="noreferrer" href="https://github.com/zeirnox">
-                    <FontAwesomeIcon icon={faGithub} color="#FFFFFF" />
-                </a>
-            </li>
-        </ul>
-    </div>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="projects-link"
+        to="/projects"
+      >
+        <FontAwesomeIcon icon={faMugHot} color="#FFFFFF" />
+      </NavLink>
+    </nav>
+    <ul>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://github.com/zeirnox">
+          <FontAwesomeIcon icon={faGithub} color="#FFFFFF" />
+        </a>
+      </li>
+    </ul>
+  </div>
 )
 
 export default Sidebar
