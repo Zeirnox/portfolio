2f2430c68897eda0f9cd54f2f37d3802eef4e3cf
Zeirnox <84430553+Zeirnox@users.noreply.github.com>
2023-03-15 14:30:42 -0700

    Mobile update for page layouts

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,7 +6,8 @@ import './index.scss';
 const Home = () => {
     const [letterClass] = useState('text-animate')
     const nameArr = ['a', 'm', 'e', 's,']
-    const jobArr = ['a', ' ','w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
+    const jobArr = ['a', ' ','w', 'e', 'b', ' ']
+    const jobArrL = ['d', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
 
     return (
         <>
@@ -15,7 +16,7 @@ const Home = () => {
                     <h1>Hello,<br />
                         <span className={`${letterClass} _13`}>I</span>
                         <span className={`${letterClass} _14`}>'m</span>
-                        <img src={LogoTitle} alt="developer" />
+                        <img src={LogoTitle} alt="J" />
                         <AnimatedLetters letterClass={letterClass}
                             strArr={nameArr}
                             idx={15}
@@ -24,6 +25,10 @@ const Home = () => {
                         <AnimatedLetters letterClass={letterClass}
                             strArr={jobArr}
                             idx={14}
+                        />
+                        <AnimatedLetters className="jobL" letterClass={letterClass} 
+                            strArr={jobArrL}
+                            idx={20}
                         />
                     </h1>
                     <h2>HTML / CSS / Javascript</h2>
